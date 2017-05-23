b7560183c16bdd35cf1ff66432f96f4d7c32c152
Peter Frank <peter.frank@mobileforming.com>
2017-05-23 16:35:42 -0700

    gh #42 xsd update and inital impl on converter
    The XSD wasn't strictly correct as there was a cardinality mismatch between CoverateType and FileType, ti needed to be 1 to many, not 1 to 1
    Just beginning implementaiton of the JaxbCoberturaToSonarQubeReportConverter

--- conversion/src/main/java/com/qualinsight/mojo/cobertura/transformation/JaxbCoberturaToSonarQubeReportConverter.java
+++ conversion/src/main/java/com/qualinsight/mojo/cobertura/transformation/JaxbCoberturaToSonarQubeReportConverter.java
@@ -24,12 +24,15 @@ import com.qualinsight.mojo.sonar.model.FileType;
 import com.qualinsight.mojo.sonar.model.LineToCoverType;
 import com.qualinsight.mojo.sonar.model.ObjectFactory;
 
+import net.sourceforge.cobertura.coveragedata.ClassData;
+import net.sourceforge.cobertura.coveragedata.CoverageData;
 import net.sourceforge.cobertura.coveragedata.ProjectData;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 
 import java.io.File;
+import java.util.Collection;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -78,17 +81,20 @@ public class JaxbCoberturaToSonarQubeReportConverter implements CoberturaToSonar
       final CoverageType coverageType = new CoverageType();
       coverageType.setVersion(1);
 
-      final FileType fileType = new FileType();
-      fileType.setPath("/my/path");
-      coverageType.setFile(fileType);
+      @SuppressWarnings("unchecked")
+      final Collection<ClassData> classData = (Collection<ClassData>) projectData.getClasses();
+      for(ClassData classDatum : classData){
+        final FileType fileType = new FileType();
+        coverageType.getFile().add(fileType);
 
-      final LineToCoverType lineToCoverType = new LineToCoverType();
-      lineToCoverType.setBranchesToCover(1);
-      lineToCoverType.setCovered("1");
-      lineToCoverType.setLineNumber(1);
-      lineToCoverType.setCoveredBranches(1);
-      lineToCoverType.setValue("1");
-      fileType.getLineToCover().add(lineToCoverType);
+        //TODO the sourceFileName against various sourcePaths to create the full path needed by sonar
+        fileType.setPath(classDatum.getSourceFileName());
+
+
+        for(CoverageData coverageData : classDatum.getLines()){
+          //pattern match for the different coverage data types to create the LineToCoverType
+        }
+      }
 
       final JAXBElement<CoverageType> coverageTypeJAXBElement = factory.createCoverage(coverageType);
       marshaller.marshal(coverageTypeJAXBElement, destinationSonarXmlFile);
